ec81a7d0809be3a356410478528e361eabc1d2c8
ConstantRen <155212732+ConstantRen@users.noreply.github.com>
2025-03-11 08:12:32 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const API_BASE_URL = "https://54.169.163.184:8000"; // Updated API URL
+  const API_BASE_URL = "http://54.169.163.184:8000"; // Updated API URL
 
   const [users, setUsers] = useState([]);
   const [name, setName] = useState("");
