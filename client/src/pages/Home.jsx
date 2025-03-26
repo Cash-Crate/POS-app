360d92d991927d1b0149358d5fd33139e5fd0a69
Ces <francesluis23@gmail.com>
2025-03-26 20:36:02 +0800

    Added login page and logo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,20 @@
+import logo from '../../public/CashCrateLogo.webp'
+import { motion } from "framer-motion"
 
 const Home = () => {
   return (
-    <main>
-      <header>
-
+    <main className="h-screen flex flex-col">
+      <header className="head flex items-center justify-end px-4 md:px-8 py-6"> 
+        <ul class="flex gap-8 items-center">
+          <li className="underline"><a href="/login">Login</a></li>
+          <li><motion.button whileTap={{ scale: 0.95 }} className="btn-highlight w-30 py-2 rounded-2xl">Install Now</motion.button></li>
+        </ul>
       </header>
-      <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-bold">Welcome to CashCrate</h1>
-        <p className="text-xl">
-          CashCrate is a POS (Point of Sale) management system for small businesses.
-        </p>
-      </div>
+      <section className="bg-gradient h-full flex flex-col items-center gap-8 px-4 md:px-8 pt-[10vh] md:pt-[15vh]">
+        <img src={logo} alt="Cash Crate Logo" className="aspect-square"/>
+        <h1 className="text-5xl font-bold text-center">Cash Crate POS</h1>
+        <motion.button whileTap={{ scale: 0.95 }} className="btn-light w-30 py-2 rounded-2xl">Install Now</motion.button>
+      </section>
     </main>
   );
 };
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import image from '../../public/CashCrateLogo.webp'
+import { useState } from "react"
+import { motion } from "framer-motion"
+
+const Login = () => {
+  const [isActive, setActive] = useState(true);
+
+  return (
+    <main className="h-screen flex flex-col">
+      <section className="bg-gradient h-full flex items-center justify-center py-5 px-4 md:px-8 xl:px-12">
+        <div className='card flex flex-col gap-5 md:gap-5 px-5 py-7.5 lg:max-w-[500px] xl:max-w-[520px]'>
+          <div className='flex flex-col items-center gap-2.5 md:gap-5'>
+            <div className='flex flex-row items-center gap-2.5 pb-2'>
+              <img src={image} alt="Cash Crate Logo" className="aspect-square w-10 md:w-12.5 lg:w-32"/>
+              <h2>Cash Crate POS</h2>
+            </div>
+            <hr/>
+            <h4>Ready to go? <strong>Log in</strong>!</h4>
+          </div>
+          <form action="POST" className='flex flex-col gap-2.5'>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="Email">Username</label>
+              <motion.input whileTap={{ scale: 0.95 }} type="text" placeholder="your@email.com" className="input p-2 border-2 rounded-lg bg-hif" required/>
+            </div>
+            <div className='flex flex-col gap-2'>
+              <label htmlFor="password">Password</label>
+              <motion.input whileTap={{ scale: 0.95 }} type="password" placeholder="********" className="input p-2 border-2 rounded-lg" required/>
+            </div>
+          </form>
+            <motion.button whileTap={{ scale: 0.95 }} className='btn-highlight rounded-lg w-full py-2.5'>Log In</motion.button>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
