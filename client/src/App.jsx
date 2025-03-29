08e39c0fa3b33cee12de7794a02c8aa38ac2d773
JullianMQ <jullianmq@gmail.com>
2025-03-29 17:17:56 +0800

    fixed imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import Items from './pages/GetItems';
 import Login from './pages/Login';
 import Documentation from './pages/Documentation';
 import ProductsTable from './pages/Products';
-import CustomerLogs from './pages/customerlog';
-import ActionLogs from './pages/actionlog';
+import CustomerLogs from './pages/Customerlog';
+import ActionLogs from './pages/Actionlog';
 import './App.css';
 
 function App() {
