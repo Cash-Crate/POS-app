04e80d6295e66e6e51e49d163388a1514ac8ff7c
JullianMQ <jullianmq@gmail.com>
2025-03-30 23:43:10 +0800

    deleted server directory

--- server/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~/POS-app/server
-git pull origin test-branch
-docker pull 891377178047.dkr.ecr.ap-southeast-1.amazonaws.com/server:latest
-docker-compose down
-docker-compose up -d
-sudo systemctl restart nginx
