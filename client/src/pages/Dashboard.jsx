928a88866a399564774e9066ed0d178f232019e4
JullianMQ <jullianmq@gmail.com>
2025-04-01 23:24:38 +0800

    changed api address

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -58,7 +58,7 @@ const Dashboard = () => {
     const [browserConfig, setBrowserConfig] = useState({});
     
     useEffect(() => {
-        fetchWithAuth("http://localhost:3000/api/logins", {}) 
+        fetchWithAuth("https://api.cashcrate.shop/api/logins", {}) 
             .then((response) => response.json())
             .then((data) => {
                 setCrudGraph(data);  
